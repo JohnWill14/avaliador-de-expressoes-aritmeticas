59e69ccfb57b4736ad6cb9cf1de4e8fd43feaf03
John William <johnwill.v.2017@gmail.com>
2022-04-02 17:33:31 -0300

    add project initial rust

---
+++ rust/avaliador/target/debug/avaliador.d
@@ -0,0 +1,1 @@
+/home/john/Área\ de\ Trabalho/avaliador-de-expressoes-aritmeticas/rust/avaliador/target/debug/avaliador: /home/john/Área\ de\ Trabalho/avaliador-de-expressoes-aritmeticas/rust/avaliador/src/main.rs
---
+++ rust/avaliador/target/debug/deps/avaliador-c290fc63a7f7a21d.d
@@ -0,0 +1,5 @@
+/home/john/Área de Trabalho/avaliador-de-expressoes-aritmeticas/rust/avaliador/target/debug/deps/avaliador-c290fc63a7f7a21d: src/main.rs
+
+/home/john/Área de Trabalho/avaliador-de-expressoes-aritmeticas/rust/avaliador/target/debug/deps/avaliador-c290fc63a7f7a21d.d: src/main.rs
+
+src/main.rs:
