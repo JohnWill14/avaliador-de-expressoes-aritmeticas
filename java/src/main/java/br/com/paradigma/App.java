ba3ead7b51444e23a2bf2f739a5ad6725db6d2b8
John William <johnwill.v.2017@gmail.com>
2022-04-03 20:38:55 -0300

    update App

--- java/src/main/java/br/com/paradigma/App.java
+++ java/src/main/java/br/com/paradigma/App.java
@@ -1,6 +1,8 @@
 package br.com.paradigma;
 
 import br.com.paradigma.algorithm.ArithmeticExpressionEvaluator;
+import br.com.paradigma.algorithm.ConvertToken;
+import br.com.paradigma.algorithm.Tokens;
 
 import java.util.List;
 import java.util.Scanner;
@@ -9,13 +11,15 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArithmeticExpressionEvaluator algorithm = new ArithmeticExpressionEvaluator();
-        while(sc.hasNext()){
-            String expression = sc.nextLine();
-            List<String> tokens = algorithm.generateTokens(expression);
-            List<String> parser = algorithm.convertInfixExpressionToPostfix(tokens);
-            int value = algorithm.solvePostfixExpression(parser);
+        Tokens tokensUtil = new Tokens();
+        ConvertToken convertToken = new ConvertToken();
 
-            System.out.println(expression +" => "+ value);
+        while(sc.hasNext()){
+            System.out.print(">>> ");
+            String expression = sc.nextLine();
+            List<String> tokens = tokensUtil.generateTokens(expression);
+            List<String> parser = convertToken.convertInfixExpressionToPostfix(tokens);
+            int value = algorithm.solvePostfixExpression(parser);
         }
 
     }
